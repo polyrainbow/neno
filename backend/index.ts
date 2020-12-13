d6c79691874a783c57d7c27db114a0513c0bf4ed
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2020-12-13 05:00:00 +0100

    refactor: use commander for cmd argument parsing

--- backend/index.ts
+++ backend/index.ts
@@ -5,19 +5,30 @@ import * as url from "url";
 import mkdirp from "mkdirp";
 import * as config from "./config.js";
 import startApp from "./app.js";
+import { Command } from "commander";
+const program = new Command();
+program.version('0.0.1');
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
-let PORT = config.DEFAULT_PORT;
 const REPO_PATH = path.join(__dirname, "..");
-let DATA_PATH = path.join(REPO_PATH, "..", "network-notes-data");
-if (process.env.DATA_FOLDER_PATH) {
-  DATA_PATH = process.env.DATA_FOLDER_PATH;
-}
+
+program
+  .option(
+    '-p, --port <value>', 'HTTP port',
+    config.DEFAULT_PORT.toString(),
+  )
+  .option(
+    '-d, --data-folder-path <value>', "path to data folder",
+    path.join(REPO_PATH, "..", "network-notes-data"),
+  );
+
+program.parse(process.argv);
+console.log(program);
 
 // passwords and usernames must not contain colons
 let users;
-const usersFile = path.join(DATA_PATH, "users.json");
+const usersFile = path.join(program.dataFolderPath, "users.json");
 if (fs.existsSync(usersFile)) {
   console.log("Loading existing users file...");
   const json = fs.readFileSync(usersFile).toString();
@@ -26,29 +37,22 @@ if (fs.existsSync(usersFile)) {
   console.log(
     "No users file found. Creating one by myself with default users...",
   );
-  mkdirp.sync(DATA_PATH);
+  mkdirp.sync(program.dataFolderPath);
   fs.writeFileSync(usersFile, JSON.stringify(config.DEFAULT_USERS));
   users = config.DEFAULT_USERS;
 }
 
 
-const customPortArgument = process.argv.find((arg) => {
-  return arg.startsWith("port=");
-});
-
-if (customPortArgument) {
-  PORT = parseInt(customPortArgument.substring(5));
-}
-
 const app = startApp({
   users,
-  dataPath: DATA_PATH,
+  dataPath: program.dataFolderPath,
   frontendPath: path.join(REPO_PATH, "frontend"),
 });
 
+
 const httpServer = http.createServer(app);
-httpServer.listen(PORT);
-console.log("Ready on port " + PORT);
+httpServer.listen(parseInt(program.port));
+console.log("Ready on port " + program.port);
 
 
 
