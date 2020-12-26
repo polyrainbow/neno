fcd8ef19f4fe887a21c065205d8e56da0851a0fc
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2020-12-26 05:00:00 +0100

    feat: different overlay gradient for dark mode

--- frontend/app/Overlay.js
+++ frontend/app/Overlay.js
@@ -4,17 +4,6 @@ const Overlay = ({ children, onClick }) => {
   return <div
     onClick={onClick}
     id="overlay"
-    style={{
-      "position": "fixed",
-      "width": "100vw",
-      "height": "100vh",
-      "background":
-        "linear-gradient(45deg, rgba(2,0,36,1) 0%, "
-        + "rgba(9,9,121,0.5) 35%, rgba(0,212,255,1) 100%)",
-      "display": "grid",
-      "alignItems": "center",
-      "zIndex": "100",
-    }}
   >
     {children}
   </div>;
