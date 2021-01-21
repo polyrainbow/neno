e5547b577d8c2c24c0f0317eea19535c9b1c0d49
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2021-01-21 05:00:00 +0100

    fix: fix app menu toggle

--- frontend/app/OutsideAlerter.js
+++ frontend/app/OutsideAlerter.js
@@ -19,10 +19,10 @@ function useOutsideAlerter(ref, callback) {
 
   useEffect(() => {
     // Bind the event listener
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("click", handleClickOutside);
     return () => {
       // Unbind the event listener on clean up
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("click", handleClickOutside);
     };
   });
 }
