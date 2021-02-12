8bbb994ac5decf3a2f8675c7d2e7855b5e1d4393
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2021-02-12 05:00:00 +0100

    refactor: common webpack config

--- frontend/webpack.development.js
+++ frontend/webpack.development.js
@@ -1,73 +1,8 @@
-import * as path from "path";
-import * as url from "url";
+import { merge } from "webpack-merge";
+import commonConfig from "./webpack.common.js";
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-
-export default {
+const config = {
   mode: "development",
-  entry: path.join(__dirname, "app", "index.js"),
-  output: {
-    // `filename` provides a template for naming your bundles (remember to use
-    // `[name]`)
-    filename: "[name].bundle.js",
-    // `chunkFilename` provides a template for naming code-split bundles
-    // (optional)
-    chunkFilename: "[name].bundle.js",
-    // `path` is the folder where Webpack will place your bundles
-    path: path.resolve(__dirname, "assets"),
-  },
-  resolve: {
-    // Add .ts and .tsx as a resolvable extension.
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: [
-          "ts-loader",
-          path.resolve(__dirname, "ts-loader-preparation.cjs"),
-        ],
-      },
-      {
-        test: /\.js?$/,
-        include: [
-          path.resolve(__dirname, "app"),
-        ],
-        loader: "babel-loader",
-        options: {
-          presets: [
-            "@babel/preset-react",
-          ],
-          plugins: [
-            // we need these two plugins because webpack does not support
-            // that syntax yet, even though Chrome does already.
-            ["@babel/plugin-proposal-class-properties"],
-            ["@babel/plugin-proposal-private-methods"],
-          ],
-          cacheDirectory: true,
-          cacheCompression: false,
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  watch: true,
-  devServer: {
-    historyApiFallback: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers":
-        "X-Requested-With, content-type, Authorization",
-    },
-    disableHostCheck: true,
-    contentBase: __dirname,
-  },
-  watchOptions: {
-    ignored: /node_modules/,
-  },
 };
+
+export default merge(commonConfig, config);
