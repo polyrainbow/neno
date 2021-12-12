bed500fc5b5a100ac5f338c8978ed864bceac716
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2021-12-12 05:00:00 +0000

    chore: remove outdated tool "obfuscate-db"

--- tools/obfuscate-db/printRandomWikipediaArticleNames.sh
+++
@@ -1,6 +0,0 @@
-max=100
-for i in `seq 1 $max`
-do
-    curl -sL https://en.wikipedia.org/wiki/Special:Random | grep '<title>' | grep -o -P '(?<=\<title\>)[^\r^\n]*(?= - Wikipedia)'
-    sleep 4s
-done
