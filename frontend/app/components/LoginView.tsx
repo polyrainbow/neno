f872ffcb8e96d8031ff79a23a1d7761ae58ebc9c
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2022-06-11 05:00:00 +0000

    feat: new login page layout

--- frontend/app/components/LoginView.tsx
+++ frontend/app/components/LoginView.tsx
@@ -3,6 +3,8 @@ import HeaderContainer from "./HeaderContainer";
 import LoginViewServer from "./LoginViewServer";
 import LoginViewLocal from "./LoginViewLocal";
 import { DatabaseMode } from "../enum/DatabaseMode";
+import { ASSETS_PATH } from "../config";
+import { l } from "../lib/intl";
 
 const LoginView = ({
   serverDatabaseProvider,
@@ -20,14 +22,29 @@ const LoginView = ({
       toggleAppMenu={toggleAppMenu}
     />
     <section id="section_login">
-      <LoginViewServer
-        serverDatabaseProvider={serverDatabaseProvider}
-        setDatabaseMode={setDatabaseMode}
-      />
-      <LoginViewLocal
-        localDatabaseProvider={localDatabaseProvider}
-        setDatabaseMode={setDatabaseMode}
-      />
+      <div
+        id="login_welcome"
+      >
+        <img
+          src={ASSETS_PATH + "app-icon/logo-circle.svg"}
+          alt={l("login.logo.alt")}
+          id="login_logo"
+        />
+        <div id="login_welcome_app_title">{l("app.title")}</div>
+        <div>{l("login.introduction")}</div>
+      </div>
+      <div>
+        <LoginViewServer
+          serverDatabaseProvider={serverDatabaseProvider}
+          setDatabaseMode={setDatabaseMode}
+        />
+      </div>
+      <div>
+        <LoginViewLocal
+          localDatabaseProvider={localDatabaseProvider}
+          setDatabaseMode={setDatabaseMode}
+        />
+      </div>
     </section>
   </>;
 };
