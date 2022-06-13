2ed47a5131ebadf2c705bd770bd6787cad6a06bb
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2022-06-13 05:00:00 +0000

    chore: simplify release package creation

--- tools/buildReleasePackage.sh
+++ tools/buildReleasePackage.sh
@@ -5,31 +5,21 @@ builds a NENO release package
 '
 
 # this will also be the name of the directory inside the archive
-TARGET_DIRECTORY_NAME=neno
-TARGET_DIRECTORY=./$TARGET_DIRECTORY_NAME
 VERSION=v$(eval "cat ../package.json | jq -r '.version'")
+OUTPUT_FILENAME=neno-$VERSION.tar.gz
 
 cd ..
 
-echo "Removing old builds..."
+echo "Removing old builds ..."
 rm frontend/public/js/*
 rm -R dist
 
-echo "Creating build"
+echo "Creating build ..."
 npm run build
 npm run build-browser-extension
 
-echo "Removing old target directory content"
-rm -R $TARGET_DIRECTORY
-
-echo "Creating target directory"
-mkdir -p $TARGET_DIRECTORY
-
-echo "Copying files to target directory"
-rsync -av --progress . $TARGET_DIRECTORY --exclude node_modules --exclude .github --exclude .git
-
-tar -zcvf neno-$VERSION.tar.gz $TARGET_DIRECTORY
-
-rm -R $TARGET_DIRECTORY
+echo "Creating archive ..."
+# works with Mac tar, not sure about GNU tar
+tar --exclude node_modules --exclude="*.tar.gz" -zcv -f $OUTPUT_FILENAME ./*
 
 echo "Done."
