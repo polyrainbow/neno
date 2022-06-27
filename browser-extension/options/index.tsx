f8f6cbfe58ddfbd7cdef60c8cefcd5fb0cfbbc56
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2022-06-27 05:00:00 +0000

    refactor(be): use new react api

--- browser-extension/options/index.tsx
+++ browser-extension/options/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const appContainer = document.getElementById("app");
-ReactDOM.render(
+const root = createRoot(appContainer);
+root.render(
   <App />,
-  appContainer,
 );
