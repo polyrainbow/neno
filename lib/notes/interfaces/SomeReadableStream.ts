9aa28f8e29496e1435f377abc523fff2279197ef
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2022-07-28 05:00:00 +0000

    docs(notes): update comment

--- lib/notes/interfaces/SomeReadableStream.ts
+++ lib/notes/interfaces/SomeReadableStream.ts
@@ -1,3 +1,7 @@
-// SomeReadableStream can be anything, depending in
-// what kind of environment the Notes module is started
+/* 
+  SomeReadableStream can be anything, depending on
+  what kind of environment the Notes module is started in
+  E. g. it could be ReadableStream or NodeJS.ReadStream, etc.
+  As these types are quite different so we do not use a union type.
+*/
 export type SomeReadableStream = any;
