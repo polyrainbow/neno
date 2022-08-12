b9a49d52115a7241f5fd94b6445539d63eeb89d0
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2022-08-12 05:00:00 +0000

    refactor: use node argument parsing
    as described in https://2ality.com/2022/08/node-util-parseargs.html#the-steps-involved-in-processing-command-line-arguments

--- backend/index.ts
+++ backend/index.ts
@@ -4,19 +4,22 @@ import getProgramArguments from "./getProgramArguments.js";
 import User from "./interfaces/User.js";
 import Users from "./users.js";
 import getUrlMetadata from "./lib/getUrlMetadata.js";
-import { REPO_PATH, VERSION, SERVER_TIMEOUT } from "./config.js";
+import { REPO_PATH, SERVER_TIMEOUT } from "./config.js";
 import * as logger from "./lib/logger.js";
 import startServer from "./server.js";
 import checkDataDirectory from "./checkDataDirectory.js";
 
 logger.info("💡 Starting NENO...");
 
-const programArguments = getProgramArguments(VERSION);
+const programArguments = getProgramArguments();
 
-if (programArguments.urlMetadata.length > 0) {
-  logger.info("Grabbing url metadata for " + programArguments.urlMetadata);
+logger.verbose("Program arguments:");
+logger.verbose(JSON.stringify(programArguments, null, "  "));
+
+if (programArguments["url-metadata"].length > 0) {
+  logger.info("Grabbing url metadata for " + programArguments["url-metadata"]);
   try {
-    await getUrlMetadata(programArguments.urlMetadata);
+    await getUrlMetadata(programArguments["url-metadata"]);
   } catch (e) {
     logger.error(e);
   }
@@ -25,37 +28,37 @@ if (programArguments.urlMetadata.length > 0) {
 
 logger.info("💡 Checking data directory...");
 
-await checkDataDirectory(programArguments.dataFolderPath);
+await checkDataDirectory(programArguments["data-folder-path"]);
 
 logger.info("💡 Getting users...");
 
-await Users.init(programArguments.dataFolderPath);
+await Users.init(programArguments["data-folder-path"]);
 const users: User[] = Users.getAll();
 
 logger.info("💡 Starting app...");
 
 const app = await startApp({
   users,
-  dataPath: programArguments.dataFolderPath,
+  dataPath: programArguments["data-folder-path"],
   frontendPath: path.join(REPO_PATH, "frontend", "public"),
-  sessionSecret: programArguments.sessionSecret,
-  sessionTTL: parseInt(programArguments.sessionTtl),
-  maxUploadFileSize: parseInt(programArguments.maxUploadFileSize),
-  sessionCookieName: programArguments.sessionCookieName,
-  maxGraphSize: programArguments.maxGraphSize,
+  sessionSecret: programArguments["session-secret"],
+  sessionTTL: programArguments["session-ttl"],
+  maxUploadFileSize: programArguments["max-upload-file-size"],
+  sessionCookieName: programArguments["session-cookie-name"],
+  maxGraphSize: programArguments["max-graph-size"],
 });
 
 logger.info("💡 Starting server...");
 
 await startServer({
   app,
-  certKeyPath: programArguments.certKeyPath,
-  certPath: programArguments.certPath,
-  useHttps: programArguments.useHttps,
-  httpPort: parseInt(programArguments.httpPort),
-  httpsPort: parseInt(programArguments.httpsPort),
+  certKeyPath: programArguments["cert-key-path"],
+  certPath: programArguments["cert-path"],
+  useHttps: programArguments["use-https"],
+  httpPort: programArguments["http-port"],
+  httpsPort: programArguments["https-port"],
   timeout: SERVER_TIMEOUT,
-  ipv6Only: programArguments.ipv6Only,
+  ipv6Only: programArguments["ipv6-only"],
 });
 
 logger.info("✅ Ready.");
