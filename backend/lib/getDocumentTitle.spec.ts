973246513452034c2ec61f338b087b10ff9bfac8
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2022-08-28 05:00:00 +0000

    fix(backend): document title regex

---
+++ backend/lib/getDocumentTitle.spec.ts
@@ -0,0 +1,17 @@
+import assert from 'node:assert';
+import it, { describe } from 'node:test';
+import { getDocumentTitleFromHtml } from './getDocumentTitle.js';
+
+describe("getDocumentTitleFromHtml", () => {
+  it("should correctly parse title elements with attributes", async () => {
+    const input = `<html lang="en">
+    <head>
+      <title data-attribute="something">Title</title>
+    </head>
+    <body>
+    </body>
+  </html>`;
+
+    assert.strictEqual(getDocumentTitleFromHtml(input), "Title");
+  });
+});
