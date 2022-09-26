09a20a65613783bdd5888414bc5c6c14a3604c24
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2022-09-26 05:00:00 +0000

    feat(frontend): update search presets dialog

--- frontend/app/components/SearchDialog.tsx
+++ frontend/app/components/SearchDialog.tsx
@@ -2,6 +2,22 @@ import React from "react";
 import { l } from "../lib/intl";
 import Dialog from "./Dialog";
 
+
+const SearchPresetDialogPreset = ({
+  label,
+  searchValue,
+  onClick,
+}) => {
+  return <p>
+    <button
+      onClick={onClick}
+      className="small-button dialog-box-button"
+    >{searchValue}</button>
+    {label}
+  </p>;
+};
+
+
 const SearchDialog = ({
   setSearchValue,
   onClose,
@@ -13,53 +29,54 @@ const SearchDialog = ({
 
   return <Dialog
     onClickOnOverlay={onClose}
-    className="stats-dialog"
+    className="search-presets-dialog"
   >
     <h1>{l("list.search.presets")}</h1>
-    <p>
-      <button
-        onClick={() => setSearchValueAndClose("exact:")}
-        className="default-button dialog-box-button"
-      >{l("list.search.presets.untitled-notes")}</button>
-    </p>
-    <h2>{l("list.search.presets.duplicates")}</h2>
-    <p>
-      <button
-        onClick={() => setSearchValueAndClose("duplicates:title")}
-        className="default-button dialog-box-button"
-      >{l("list.search.presets.notes-with-duplicate-titles")}</button>
-    </p>
-    <p>
-      <button
-        onClick={() => setSearchValueAndClose("duplicates:url")}
-        className="default-button dialog-box-button"
-      >{l("list.search.presets.notes-with-duplicate-urls")}</button>
-    </p>
-    <h2>Types</h2>
-    <p>
-      <button
-        onClick={() => setSearchValueAndClose("has-media:audio")}
-        className="default-button dialog-box-button"
-      >{l("list.search.presets.notes-with-audio")}</button>
-    </p>
-    <p>
-      <button
-        onClick={() => setSearchValueAndClose("has-media:video")}
-        className="default-button dialog-box-button"
-      >{l("list.search.presets.notes-with-video")}</button>
-    </p>
-    <p>
-      <button
-        onClick={() => setSearchValueAndClose("has-media:pdf")}
-        className="default-button dialog-box-button"
-      >{l("list.search.presets.notes-with-pdfs")}</button>
-    </p>
-    <p>
-      <button
-        onClick={() => setSearchValueAndClose("has-media:image")}
-        className="default-button dialog-box-button"
-      >{l("list.search.presets.notes-with-images")}</button>
-    </p>
+    <SearchPresetDialogPreset
+      onClick={() => setSearchValueAndClose("exact:")}
+      label={l("list.search.presets.untitled-notes")}
+      searchValue={"exact:"}
+    />
+    <SearchPresetDialogPreset
+      onClick={() => setSearchValueAndClose("duplicates:title")}
+      label={l("list.search.presets.notes-with-duplicate-titles")}
+      searchValue={"duplicates:title"}
+    />
+    <SearchPresetDialogPreset
+      onClick={() => setSearchValueAndClose("duplicates:url")}
+      label={l("list.search.presets.notes-with-duplicate-urls")}
+      searchValue={"duplicates:url"}
+    />
+    <SearchPresetDialogPreset
+      onClick={() => setSearchValueAndClose("has-media:audio")}
+      label={l("list.search.presets.notes-with-audio")}
+      searchValue={"has-media:audio"}
+    />
+    <SearchPresetDialogPreset
+      onClick={() => setSearchValueAndClose("has-media:video")}
+      label={l("list.search.presets.notes-with-video")}
+      searchValue={"has-media:video"}
+    />
+    <SearchPresetDialogPreset
+      onClick={() => setSearchValueAndClose("has-media:pdf")}
+      label={l("list.search.presets.notes-with-pdfs")}
+      searchValue={"has-media:pdf"}
+    />
+    <SearchPresetDialogPreset
+      onClick={() => setSearchValueAndClose("has-media:image")}
+      label={l("list.search.presets.notes-with-images")}
+      searchValue={"has-media:image"}
+    />
+    <SearchPresetDialogPreset
+      onClick={() => setSearchValueAndClose("has-block:list")}
+      label={l("list.search.presets.has-list")}
+      searchValue={"has-block:list"}
+    />
+    <SearchPresetDialogPreset
+      onClick={() => setSearchValueAndClose("has:custom-metadata")}
+      label={l("list.search.presets.has-custom-metadata")}
+      searchValue={"has:custom-metadata"}
+    />
     <button
       onClick={onClose}
       className="default-button dialog-box-button default-action"
