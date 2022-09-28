74a275b96b3665162290e48269a0d0517ce85f4f
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2022-09-28 05:00:00 +0000

    refactor: object for search presets

--- frontend/app/components/SearchDialog.tsx
+++ frontend/app/components/SearchDialog.tsx
@@ -2,17 +2,62 @@ import React from "react";
 import { l } from "../lib/intl";
 import Dialog from "./Dialog";
 
+interface SearchPreset {
+  label: string,
+  query: string,
+}
+
+
+const DEFAULT_SEARCH_PRESETS: SearchPreset[] = [
+  {
+    "label": l("list.search.presets.untitled-notes"),
+    "query": "exact:",
+  },
+  {
+    "label": l("list.search.presets.notes-with-duplicate-titles"),
+    "query": "duplicates:title",
+  },
+  {
+    "label": l("list.search.presets.notes-with-duplicate-urls"),
+    "query": "duplicates:url",
+  },
+  {
+    "label": l("list.search.presets.notes-with-audio"),
+    "query": "has-media:audio",
+  },
+  {
+    "label": l("list.search.presets.notes-with-video"),
+    "query": "has-media:video",
+  },
+  {
+    "label": l("list.search.presets.notes-with-pdfs"),
+    "query": "has-media:pdf",
+  },
+  {
+    "label": l("list.search.presets.notes-with-images"),
+    "query": "has-media:image",
+  },
+  {
+    "label": l("list.search.presets.has-list"),
+    "query": "has-block:list",
+  },
+  {
+    "label": l("list.search.presets.has-custom-metadata"),
+    "query": "has:custom-metadata",
+  },
+];
+
 
 const SearchPresetDialogPreset = ({
   label,
-  searchValue,
+  query,
   onClick,
 }) => {
   return <p>
     <button
       onClick={onClick}
       className="small-button dialog-box-button"
-    >{searchValue}</button>
+    >{query}</button>
     {label}
   </p>;
 };
@@ -22,6 +67,8 @@ const SearchDialog = ({
   setSearchValue,
   onClose,
 }) => {
+  const searchPresets = DEFAULT_SEARCH_PRESETS;
+
   const setSearchValueAndClose = (searchValue) => {
     setSearchValue(searchValue);
     onClose();
@@ -32,51 +79,16 @@ const SearchDialog = ({
     className="search-presets-dialog"
   >
     <h1>{l("list.search.presets")}</h1>
-    <SearchPresetDialogPreset
-      onClick={() => setSearchValueAndClose("exact:")}
-      label={l("list.search.presets.untitled-notes")}
-      searchValue={"exact:"}
-    />
-    <SearchPresetDialogPreset
-      onClick={() => setSearchValueAndClose("duplicates:title")}
-      label={l("list.search.presets.notes-with-duplicate-titles")}
-      searchValue={"duplicates:title"}
-    />
-    <SearchPresetDialogPreset
-      onClick={() => setSearchValueAndClose("duplicates:url")}
-      label={l("list.search.presets.notes-with-duplicate-urls")}
-      searchValue={"duplicates:url"}
-    />
-    <SearchPresetDialogPreset
-      onClick={() => setSearchValueAndClose("has-media:audio")}
-      label={l("list.search.presets.notes-with-audio")}
-      searchValue={"has-media:audio"}
-    />
-    <SearchPresetDialogPreset
-      onClick={() => setSearchValueAndClose("has-media:video")}
-      label={l("list.search.presets.notes-with-video")}
-      searchValue={"has-media:video"}
-    />
-    <SearchPresetDialogPreset
-      onClick={() => setSearchValueAndClose("has-media:pdf")}
-      label={l("list.search.presets.notes-with-pdfs")}
-      searchValue={"has-media:pdf"}
-    />
-    <SearchPresetDialogPreset
-      onClick={() => setSearchValueAndClose("has-media:image")}
-      label={l("list.search.presets.notes-with-images")}
-      searchValue={"has-media:image"}
-    />
-    <SearchPresetDialogPreset
-      onClick={() => setSearchValueAndClose("has-block:list")}
-      label={l("list.search.presets.has-list")}
-      searchValue={"has-block:list"}
-    />
-    <SearchPresetDialogPreset
-      onClick={() => setSearchValueAndClose("has:custom-metadata")}
-      label={l("list.search.presets.has-custom-metadata")}
-      searchValue={"has:custom-metadata"}
-    />
+    {
+      searchPresets.map((preset) => {
+        return <SearchPresetDialogPreset
+          key={preset.query + "__" + preset.label}
+          onClick={() => setSearchValueAndClose(preset.query)}
+          label={preset.label}
+          query={preset.query}
+        />;
+      })
+    }
     <button
       onClick={onClose}
       className="default-button dialog-box-button default-action"
