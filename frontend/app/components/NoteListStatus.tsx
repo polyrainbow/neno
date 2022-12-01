674632dc65c829d3c60fbbe9b38acb5e14e21df1
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2022-12-01 05:00:00 +0000

    feat: use BusyIndicator for note list

--- frontend/app/components/NoteListStatus.tsx
+++ frontend/app/components/NoteListStatus.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { l } from "../lib/intl";
 import { getIconSrc } from "../lib/utils";
+import BusyIndicator from "./BusyIndicator";
 
 const NoteListStatus = ({
   status,
 }) => {
   const map = {
-    BUSY: {
-      label: l("list.status.busy"),
-      icon: "pending",
-    },
     SEARCH_VALUE_TOO_SHORT: {
       label: l("list.status.too-short"),
       icon: "looks_3",
@@ -25,12 +22,19 @@ const NoteListStatus = ({
   return <div
     className="splash-message"
   >
-    <img
-      src={getIconSrc(activeState.icon)}
-      alt={activeState.label}
-      className="svg-icon"
-    />
-    <p>{activeState.label}</p>
+    {
+      status === "BUSY"
+        ? <BusyIndicator alt={l("list.status.busy")} height={64} />
+        : <>
+          <img
+            src={getIconSrc(activeState.icon)}
+            alt={activeState.label}
+            className="svg-icon"
+          />
+          <p>{activeState.label}</p>
+        </>
+    }
+
   </div>;
 };
 
