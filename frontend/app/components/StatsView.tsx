76bd427ad2bf240199d227d7399fc16a56c3fde3
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2022-12-07 05:00:00 +0000

    feat(frontend): stats view style update
    - remove emojis for files, as files can be more than audio/video/image/document
    - remove emoji for pins to make styling consistent
    - use new component for metadata
    - remove AppMenu code comments

--- frontend/app/components/StatsView.tsx
+++ frontend/app/components/StatsView.tsx
@@ -1,16 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { emojis } from "../config";
-import {
-  makeTimestampHumanReadable,
-  humanFileSize,
-} from "../lib/utils";
 import StatsViewAnalysisTable from "./StatsViewAnalysisTable";
 import { l } from "../lib/intl";
 import GraphStats from "../../../lib/notes/interfaces/GraphStats";
 import HeaderContainerLeftRight from "./HeaderContainerLeftRight";
-// import AppMenu from "./AppMenu";
 import useGraphId from "../hooks/useGraphId";
 import useDatabaseProvider from "../hooks/useDatabaseProvider";
+import BusyIndicator from "./BusyIndicator";
+import StatsViewMetadataTable from "./StatsViewMetadataTable";
 
 const StatsView = () => {
   const databaseProvider = useDatabaseProvider();
@@ -29,8 +25,6 @@ const StatsView = () => {
     updateStats();
   }, [databaseProvider]);
 
-  if (!graphId) return null;
-
   // @ts-ignore calling constructor via instance
   const databaseType = databaseProvider.constructor.type;
 
@@ -42,62 +36,22 @@ const StatsView = () => {
         stats !== null
           ? <>
             <h2>{l("stats.metadata")}</h2>
-            <table className="data-table stats-table">
-              <tbody>
-                <tr>
-                  <td>{l("stats.metadata.id")}</td>
-                  <td>{stats.id}</td>
-                </tr>
-                <tr>
-                  <td>{l("stats.metadata.type")}</td>
-                  <td>{databaseType}</td>
-                </tr>
-                <tr>
-                  <td>{l("stats.metadata.created-at")}</td>
-                  <td>{makeTimestampHumanReadable(stats.createdAt)}</td>
-                </tr>
-                <tr>
-                  <td>{l("stats.metadata.updated-at")}</td>
-                  <td>{makeTimestampHumanReadable(stats.updatedAt)}</td>
-                </tr>
-                <tr>
-                  <td>{l("stats.metadata.graph-size-without-files")}</td>
-                  <td>{humanFileSize(stats.size.graph)}</td>
-                </tr>
-                <tr>
-                  <td>{l("stats.metadata.size-of-all-files")}</td>
-                  <td>{humanFileSize(stats.size.files)}</td>
-                </tr>
-                <tr>
-                  <td>
-                    {emojis.document}{emojis.image}{emojis.audio}{emojis.video}
-                    <span> </span>{l("stats.metadata.number-of-files")}
-                  </td>
-                  <td>{stats.numberOfFiles.toLocaleString()}</td>
-                </tr>
-                <tr>
-                  <td>{emojis.pin} {l("stats.metadata.pins")}</td>
-                  <td>{stats.numberOfPins.toLocaleString()}</td>
-                </tr>
-              </tbody>
-            </table>
+            <StatsViewMetadataTable
+              stats={stats}
+              databaseType={databaseType}
+            />
             <h2>{l("stats.analysis")}</h2>
             <StatsViewAnalysisTable
               stats={stats}
               graphId={graphId}
             />
           </>
-          : <p>{l("stats.fetching")}</p>
+          : <BusyIndicator
+            alt={l("stats.fetching")}
+            height={64}
+          />
       }
     </section>
-    {/* <AppMenu
-      onClose={() => setIsAppMenuOpen(false)}
-      databaseProvider={databaseProvider}
-      createOneNotePerLine={createOneNotePerLine}
-      switchGraphs={switchGraphs}
-      createNewNote={createNewNote}
-      graphId={activeGraphId}
-    /> */}
   </>;
 };
 
---
+++ frontend/app/components/StatsViewMetadataTable.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { l } from "../lib/intl";
+import {
+  makeTimestampHumanReadable,
+  humanFileSize,
+} from "../lib/utils";
+
+const StatsViewMetadataTable = ({ stats, databaseType }) => {
+  return <table className="data-table stats-table">
+    <tbody>
+      <tr>
+        <td>{l("stats.metadata.id")}</td>
+        <td>{stats.id}</td>
+      </tr>
+      <tr>
+        <td>{l("stats.metadata.type")}</td>
+        <td>{databaseType}</td>
+      </tr>
+      <tr>
+        <td>{l("stats.metadata.created-at")}</td>
+        <td>{makeTimestampHumanReadable(stats.createdAt)}</td>
+      </tr>
+      <tr>
+        <td>{l("stats.metadata.updated-at")}</td>
+        <td>{makeTimestampHumanReadable(stats.updatedAt)}</td>
+      </tr>
+      <tr>
+        <td>{l("stats.metadata.graph-size-without-files")}</td>
+        <td>{humanFileSize(stats.size.graph)}</td>
+      </tr>
+      <tr>
+        <td>{l("stats.metadata.size-of-all-files")}</td>
+        <td>{humanFileSize(stats.size.files)}</td>
+      </tr>
+      <tr>
+        <td>{l("stats.metadata.number-of-files")}</td>
+        <td>{stats.numberOfFiles.toLocaleString()}</td>
+      </tr>
+      <tr>
+        <td>{l("stats.metadata.pins")}</td>
+        <td>{stats.numberOfPins.toLocaleString()}</td>
+      </tr>
+    </tbody>
+  </table>;
+};
+
+export default StatsViewMetadataTable;
