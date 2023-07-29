fa5dc1a71cb6d5b9b084e369d3dc6e964dc86311
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2023-07-29 05:00:00 +0000

    feat(editor): more liberal URL recognition

--- src/lib/editor/plugins/LinkPlugin.tsx
+++ src/lib/editor/plugins/LinkPlugin.tsx
@@ -12,8 +12,11 @@ import {
   createLinkMatcherWithRegExp,
 } from "@lexical/react/LexicalAutoLinkPlugin";
 
+// Longest TLD I have found was .international with 13 chars
+// Let's allow commas in path for scroll-to-text fragments like
+// #:~:text=Hey,there.
 const URL_REGEX
-  = /((https?:\/\/(www\.)?)|(www\.))[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+  = /((https?:\/\/(www\.)?)|(www\.))[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,13}\b([-a-zA-Z0-9()@:%_+.~#?&//=,]*)/;
 
 const EMAIL_REGEX
   = /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
