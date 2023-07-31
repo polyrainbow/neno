29f3be6b4a53f12913055a48148042b31d6f91d8
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2023-07-31 05:00:00 +0000

    first commit

---
+++ assets/javascript-DxoYEdDf.js
@@ -0,0 +1,79 @@
+import { conf as conf$1, language as language$1 } from './typescript-C6BonCYl.js';
+import './editor.main-BIz7seIw.js';
+import './index-BPCkJ68w.js';
+
+/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/
+
+var conf = conf$1;
+var language = {
+  // Set defaultToken to invalid to see what you do not tokenize yet
+  defaultToken: "invalid",
+  tokenPostfix: ".js",
+  keywords: [
+    "break",
+    "case",
+    "catch",
+    "class",
+    "continue",
+    "const",
+    "constructor",
+    "debugger",
+    "default",
+    "delete",
+    "do",
+    "else",
+    "export",
+    "extends",
+    "false",
+    "finally",
+    "for",
+    "from",
+    "function",
+    "get",
+    "if",
+    "import",
+    "in",
+    "instanceof",
+    "let",
+    "new",
+    "null",
+    "return",
+    "set",
+    "static",
+    "super",
+    "switch",
+    "symbol",
+    "this",
+    "throw",
+    "true",
+    "try",
+    "typeof",
+    "undefined",
+    "var",
+    "void",
+    "while",
+    "with",
+    "yield",
+    "async",
+    "await",
+    "of"
+  ],
+  typeKeywords: [],
+  operators: language$1.operators,
+  symbols: language$1.symbols,
+  escapes: language$1.escapes,
+  digits: language$1.digits,
+  octaldigits: language$1.octaldigits,
+  binarydigits: language$1.binarydigits,
+  hexdigits: language$1.hexdigits,
+  regexpctl: language$1.regexpctl,
+  regexpesc: language$1.regexpesc,
+  tokenizer: language$1.tokenizer
+};
+
+export { conf, language };
