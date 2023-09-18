24203cdd797d70db112e6c703a7b4cd209b58b44
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2023-09-18 05:00:00 +0000

    fix: generate dynamic manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import topLevelAwait from "vite-plugin-top-level-await";
+import { VitePWA } from "vite-plugin-pwa";
+
+const BASEPATH = "/";
 
 const wasmContentTypePlugin = {
   name: "wasm-content-type-plugin",
@@ -26,6 +29,25 @@ export default defineConfig({
       promiseImportName: (i) => `__tla_${i}`,
     }),
     wasmContentTypePlugin,
+    // eslint-disable-next-line new-cap
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        "name": "NENO",
+        "theme_color": "#ff8598",
+        "background_color": "#46b8ff",
+        "display": "standalone",
+        "start_url": ".",
+        "icons": [
+          {
+            "src": `${BASEPATH}assets/app-icon/logo.svg`,
+            "sizes": "48x48 72x72 96x96 128x128 256x256 512x512",
+            "type": "image/svg+xml",
+            "purpose": "any",
+          },
+        ],
+      },
+    }),
   ],
   worker: {
     format: "es",
@@ -34,5 +56,5 @@ export default defineConfig({
     target: "esnext",
     minify: false,
   },
-  base: "/",
+  base: BASEPATH,
 });
