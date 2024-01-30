18917ad0b8f401f01f06d551d8a112ba6e777d76
Sebastian Zimmer <SebastianZimmer@users.noreply.github.com>
2024-01-30 05:00:00 +0000

    feat: improve margins between different blocks

--- tests/visual-regression/editor.spec.ts
+++ tests/visual-regression/editor.spec.ts
@@ -26,16 +26,23 @@ const setDefaultDate = async (page: Page): Promise<void> => {
   }`);
 };
 
-const DEMO_NOTE = `# Welcome to NENO
+const TEST_NOTE = `# Heading
 Paragraph block
-
-> Quote block.
-- List item
-1. Numbered list item
+> quote block
+> another quote block
+paragraph 1
+paragraph 2 with \`inline code\`
+# Heading 2
+> another quote
+paragraph
+- list item 1 [[Non-existing wikilink]]
+- list item 2 https://example.com
+paragraph
 \`\`\`
-code block
-
-\`\`\``;
+code line 1
+code line 2
+\`\`\`
+paragraph`;
 
 test.beforeEach(async ({ page }) => {
   await setDefaultDate(page); // needs to be added before page.goto
@@ -48,32 +55,44 @@ test.beforeEach(async ({ page }) => {
 
 test.describe("Editor", () => {
   test("should look fine empty - light", async ({ page }) => {
-    expect(await page.screenshot()).toMatchSnapshot("editor-light.png");
+    expect(await page.screenshot()).toMatchSnapshot("editor-empty-light.png");
   });
 
   test("should look fine empty - dark", async ({ page }) => {
     page.emulateMedia({ colorScheme: "dark" });
-    expect(await page.screenshot()).toMatchSnapshot("editor-dark.png");
+    expect(await page.screenshot()).toMatchSnapshot("editor-empty-dark.png");
   });
 
   test("existing notes - light", async ({ page }) => {
     page.emulateMedia({ colorScheme: "light" });
-    await page.keyboard.type(DEMO_NOTE);
+    await page.keyboard.type("Note 1");
     await page.click("#button_upload");
     await page.click("#button_new");
-    await page.keyboard.type(DEMO_NOTE);
+    await page.keyboard.type("Note 2");
     await page.click("#button_upload");
     expect(await page.screenshot()).toMatchSnapshot("editor-existing-notes-light.png");
   });
 
   test("existing notes - dark", async ({ page }) => {
     page.emulateMedia({ colorScheme: "dark" });
-    await page.keyboard.type(DEMO_NOTE);
+    await page.keyboard.type("Note 1");
     await page.click("#button_upload");
     await page.click("#button_new");
-    await page.keyboard.type(DEMO_NOTE);
+    await page.keyboard.type("Note 2");
     await page.click("#button_upload");
     expect(await page.screenshot()).toMatchSnapshot("editor-existing-notes-dark.png");
+  });
+
+  test("test note - light", async ({ page }) => {
+    page.emulateMedia({ colorScheme: "light" });
+    await page.keyboard.type(TEST_NOTE);
+    expect(await page.screenshot()).toMatchSnapshot("editor-test-note-light.png");
+  });
+
+  test("test note - dark", async ({ page }) => {
+    page.emulateMedia({ colorScheme: "dark" });
+    await page.keyboard.type(TEST_NOTE);
+    expect(await page.screenshot()).toMatchSnapshot("editor-test-note-dark.png");
   });
 
   test("search presets - light", async ({ page }) => {
