484abf974d4860368bcbde67a44272b8df5e72c5
Sebastian <polyrainbow@users.noreply.github.com>
2024-02-22 05:00:00 +0000

    refactor: remove unused CAN_USE_DOM flag

--- src/lib/editor/utils/environment.ts
+++ src/lib/editor/utils/environment.ts
@@ -6,8 +6,6 @@
  *
  */
 
-import { CAN_USE_DOM } from "./canUseDom";
-
 declare global {
   interface Document {
     documentMode?: unknown;
@@ -19,31 +17,29 @@ declare global {
 }
 
 const documentMode
-  = CAN_USE_DOM && "documentMode" in document ? document.documentMode : null;
+  = "documentMode" in document ? document.documentMode : null;
 
 export const IS_APPLE: boolean
-  = CAN_USE_DOM && /Mac|iPod|iPhone|iPad/.test(navigator.platform);
+  = /Mac|iPod|iPhone|iPad/.test(navigator.platform);
 
 export const IS_FIREFOX: boolean
-  = CAN_USE_DOM && /^(?!.*Seamonkey)(?=.*Firefox).*/i.test(navigator.userAgent);
+  = /^(?!.*Seamonkey)(?=.*Firefox).*/i.test(navigator.userAgent);
 
 export const CAN_USE_BEFORE_INPUT: boolean
-  = CAN_USE_DOM && "InputEvent" in window && !documentMode
+  = "InputEvent" in window && !documentMode
     ? "getTargetRanges" in new window.InputEvent("input")
     : false;
 
 export const IS_SAFARI: boolean
-  = CAN_USE_DOM && /Version\/[\d.]+.*Safari/.test(navigator.userAgent);
+  = /Version\/[\d.]+.*Safari/.test(navigator.userAgent);
 
 export const IS_IOS: boolean
-  = CAN_USE_DOM
-  && /iPad|iPhone|iPod/.test(navigator.userAgent)
+  = /iPad|iPhone|iPod/.test(navigator.userAgent)
   && !window.MSStream;
 
 export const IS_CHROME: boolean
-  = CAN_USE_DOM && /^(?=.*Chrome).*/i.test(navigator.userAgent);
+  = /^(?=.*Chrome).*/i.test(navigator.userAgent);
 
 export const IS_APPLE_WEBKIT
-  = CAN_USE_DOM
-  && /AppleWebKit\/[\d.]+/.test(navigator.userAgent)
+  = /AppleWebKit\/[\d.]+/.test(navigator.userAgent)
   && !IS_CHROME;
