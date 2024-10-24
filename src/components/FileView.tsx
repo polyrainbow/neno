9b9ccf65489796939c1f72f1b9bdf6b4b4341075
Sebastian <5648131+polyrainbow@users.noreply.github.com>
2024-10-24 05:00:00 +0000

    refactor: outsource FileView rename form

--- src/components/FileView.tsx
+++ src/components/FileView.tsx
@@ -22,13 +22,8 @@ import {
 import HeaderContainerLeftRight from "./HeaderContainerLeftRight";
 import useNotesProvider from "../hooks/useNotesProvider";
 import {
-  getExtensionFromFilename,
   getMediaTypeFromFilename,
-  removeExtensionFromFilename,
 } from "../lib/notes/utils";
-import {
-  isValidSlug,
-} from "../lib/notes/slugUtils";
 import {
   getObjectUrlForArbitraryGraphFile,
   saveFile,
@@ -36,6 +31,7 @@ import {
 import useConfirm from "../hooks/useConfirm";
 import FileViewPreview from "./FileViewPreview";
 import HeaderButton from "./HeaderButton";
+import FileViewRenameForm from "./FileViewRenameForm";
 
 const FileView = () => {
   const notesProvider = useNotesProvider();
@@ -45,16 +41,6 @@ const FileView = () => {
   const [notes, setNotes] = useState<NoteListItem[] | null>(null);
   const [text, setText] = useState<string>("");
   const { slug } = useParams();
-  const [slugRenameInput, setSlugRenameInput] = useState<string>(
-    slug ? removeExtensionFromFilename(slug) : "",
-  );
-  const extension = fileInfo ? getExtensionFromFilename(fileInfo.filename) : "";
-  const potentialNewSlug = slugRenameInput + (
-    typeof extension === "string"
-      ? `.${extension}`
-      : ""
-  );
-  const [updateReferences, setUpdateReferences] = useState(true);
 
   const navigate = useNavigate();
 
@@ -237,92 +223,30 @@ const FileView = () => {
             height={30}
           />
       }
-      <h2>{l("files.rename")}</h2>
-      <div className="rename">
-        <div className="rename-section-input-line">
-          <label htmlFor="file-slug-rename-input">
-            {l("files.rename.new-slug")}:
-          </label>
-          <input
-            id="file-slug-rename-input"
-            type="text"
-            value={slugRenameInput}
-            onInput={(e) => {
-              const element = e.currentTarget;
-              const newValue = element.value.replace(
-                // In the input field, we also allow \p{SK} modifiers, as
-                // they are used to create a full letter with modifier via a
-                // composition session. They are not valid slug characters on
-                // their own, though.
-                // We also allow apostrophes ('), as they might be used as a
-                // dead key for letters like é.
-                // Unfortunately, it seems like we cannot simulate pressing
-                // dead keys in Playwright currently, so we cannot
-                // add a meaningful test for this.
-                /[^\p{L}\p{Sk}\d\-/._']/gu,
-                "",
-              ).toLowerCase();
-              setSlugRenameInput(newValue);
+      {
+        fileInfo
+          ? <FileViewRenameForm
+            fileInfo={fileInfo}
+            setFileInfo={async (newFileInfo: FileInfo) => {
+              const objectUrl = await getObjectUrlForArbitraryGraphFile(
+                newFileInfo,
+              );
+              setFileInfo(newFileInfo);
+              setObjectUrl(objectUrl);
+
+              navigate(getAppPath(
+                PathTemplate.FILE,
+                new Map([
+                  ["GRAPH_ID", LOCAL_GRAPH_ID],
+                  ["FILE_SLUG", newFileInfo.slug],
+                ]),
+              ), {
+                replace: true,
+              });
             }}
-          />{
-            typeof extension === "string"
-              ? `.${extension}`
-              : ""
-          }
-        </div>
-        <div className="update-references">
-          <label className="switch">
-            <input
-              type="checkbox"
-              checked={updateReferences}
-              onChange={(e) => {
-                setUpdateReferences(e.target.checked);
-              }}
-            />
-            <span className="slider round"></span>
-          </label>
-          <span className="update-references-toggle-text">
-            {l("note.slug.update-references")}
-          </span>
-        </div>
-        <button
-          disabled={
-            slugRenameInput === removeExtensionFromFilename(slug ?? "")
-            || !isValidSlug(potentialNewSlug)
-          }
-          className="default-button-small dangerous-action"
-          onClick={async () => {
-            if (
-              !slug
-              || slugRenameInput === slug
-              || !isValidSlug(potentialNewSlug)
-            ) return;
-            const newFileInfo = await notesProvider.renameFileSlug(
-              slug,
-              potentialNewSlug,
-              updateReferences,
-            );
-
-            const objectUrl = await getObjectUrlForArbitraryGraphFile(
-              newFileInfo,
-            );
-            setFileInfo(newFileInfo);
-            setObjectUrl(objectUrl);
-
-            navigate(getAppPath(
-              PathTemplate.FILE,
-              new Map([
-                ["GRAPH_ID", LOCAL_GRAPH_ID],
-                ["FILE_SLUG", potentialNewSlug],
-              ]),
-            ), {
-              replace: true,
-            });
-          }}
-        >
-          {l("files.rename")}
-        </button>
-      </div>
+          />
+          : ""
+      }
     </section>
   </>;
 };
