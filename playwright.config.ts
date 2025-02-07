bc9a14146bad519a1ddac5d76d986bdbdacd0936
Sebastian <polyrainbow@users.noreply.github.com>
2025-02-07 05:00:00 +0000

    chore: remove irrelevant comments

--- playwright.config.ts
+++ playwright.config.ts
@@ -49,29 +49,7 @@ export default defineConfig({
         viewport: { width: 1440, height: 800 },
       },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  // Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
     url: "http://localhost:5173",
