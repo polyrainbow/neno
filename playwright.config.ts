7b5837153868b8bd831da77dc2cbcea5cc94c72a
Sebastian <polyrainbow@users.noreply.github.com>
2025-08-02 05:00:00 +0000

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,12 +10,11 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left
   test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  retries: 0,
+  workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
+  failOnFlakyTests: true,
   /* Shared settings for all the projects below.
   See https://playwright.dev/docs/api/class-testoptions. */
   use: {
